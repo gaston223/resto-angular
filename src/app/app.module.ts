2ab6f33c2560409f6e8147ad8de024e5c6f135e0
gaston223 <g.coulibalyy@gmail.com>
2018-12-26 00:11:54 +0100

    autres components+routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+import { CarteComponent } from './carte/carte.component';
+import { ReserveComponent } from './reserve/reserve.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes, Router } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'carte', component: CarteComponent},
+  {path: 'reserve', component: ReserveComponent},
+  {path: 'contact', component: ContactComponent},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    HomeComponent,
+    CarteComponent,
+    ReserveComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +39,8 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
