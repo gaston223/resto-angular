a7105a0ed1e3f9138b31fb093c8dfcaa6a6ef6d8
gaston223 <g.coulibalyy@gmail.com>
2018-12-26 23:05:38 +0100

    Dashboard+Cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule } from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { CarteComponent } from './carte/carte.component';
 import { ReserveComponent } from './reserve/reserve.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { MainDashComponent } from './main-dash/main-dash.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -28,7 +29,8 @@ const routes: Routes = [
     HomeComponent,
     CarteComponent,
     ReserveComponent,
-    ContactComponent
+    ContactComponent,
+    MainDashComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +42,10 @@ const routes: Routes = [
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    RouterModule.forRoot(routes)
+    MatCardModule,
+    RouterModule.forRoot(routes),
+    MatGridListModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-dash/main-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-dash',
+  templateUrl: './main-dash.component.html',
+  styleUrls: ['./main-dash.component.css']
+})
+export class MainDashComponent {
+  cards = [
+    { title: 'A la carte', cols: 2, rows: 1 },
+    { title: 'Thieb', cols: 1, rows: 1 },
+    { title: 'Yassa', cols: 1, rows: 1 },
+    { title: 'Burger', cols: 1, rows: 1 }
+  ];
+}
